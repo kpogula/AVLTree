69b52a05f39b761373bc16b8cbad3b524c0e3e37
kpogula <kpogula2@cougarnet.uh.edu>
2022-04-12 17:39:59 -0500

    Code refactoring

--- src/test/java/avl/AvlTreeTest.java
+++ src/test/java/avl/AvlTreeTest.java
@@ -14,38 +14,38 @@ public class AvlTreeTest {
 
     @Test
     public void test01() {
-        AvlTree AvlTree0 = new AvlTree();
-        java.lang.Class<?> wildcardClass1 = AvlTree0.getClass();
+        AvlTree AvlTree = new AvlTree();
+        java.lang.Class<?> wildcardClass1 = AvlTree.getClass();
         org.junit.Assert.assertNotNull(wildcardClass1);
     }
 
     @Test
     public void test02() {
-        AvlTree avlTree = new AvlTree();
-        avlTree.insert(19);
-        avlTree.insert(15);
-        avlTree.insert(17);
-        avlTree.delete(19);
-        assertNull(avlTree.find(19));
+        AvlTree AvlTree = new AvlTree();
+        AvlTree.insert(19);
+        AvlTree.insert(15);
+        AvlTree.insert(17);
+        AvlTree.delete(19);
+        assertNull(AvlTree.find(19));
     }
 
     @Test
     public void test03() {
-        AvlTree AvlTree0 = new AvlTree();
-        AvlTree.Node node2 = AvlTree0.find((int) '4');
-        AvlTree0.delete((int) 'a');
-        java.lang.Class<?> wildcardClass5 = AvlTree0.getClass();
+        AvlTree AvlTree = new AvlTree();
+        AvlTree.Node node2 = AvlTree.find((int) '4');
+        AvlTree.delete((int) 'a');
+        java.lang.Class<?> wildcardClass5 = AvlTree.getClass();
         assertNull(node2);
         org.junit.Assert.assertNotNull(wildcardClass5);
     }
 
     @Test
     public void test04() {
-        AvlTree AvlTree0 = new AvlTree();
-        AvlTree.Node node2 = AvlTree0.find((int) '4');
-        AvlTree.Node node3 = AvlTree0.getRoot();
-        AvlTree0.delete(100);
-        java.lang.Class<?> wildcardClass6 = AvlTree0.getClass();
+        AvlTree AvlTree = new AvlTree();
+        AvlTree.Node node2 = AvlTree.find((int) '4');
+        AvlTree.Node node3 = AvlTree.getRoot();
+        AvlTree.delete(100);
+        java.lang.Class<?> wildcardClass6 = AvlTree.getClass();
         assertNull(node2);
         assertNull(node3);
         org.junit.Assert.assertNotNull(wildcardClass6);
@@ -54,15 +54,15 @@ public class AvlTreeTest {
 
     @Test
     public void test05() {
-        AvlTree AvlTree0 = new AvlTree();
+        AvlTree AvlTree = new AvlTree();
         AvlTree.Node node1 = null;
-        int int2 = AvlTree0.getBalance(node1);
-        AvlTree0.insert((int) (byte) 100);
-        AvlTree0.insert((int) 'a');
-        AvlTree.Node node7 = AvlTree0.getRoot();
+        int int2 = AvlTree.getBalance(node1);
+        AvlTree.insert((int) (byte) 100);
+        AvlTree.insert((int) 'a');
+        AvlTree.Node node7 = AvlTree.getRoot();
         // The following exception was thrown during execution in test generation
         try {
-            AvlTree0.insert((int) (short) 100);
+            AvlTree.insert((int) (short) 100);
             org.junit.Assert.fail("Expected exception of type java.lang.RuntimeException; message: duplicate Key!");
         } catch (java.lang.RuntimeException e) {
             // Expected exception.
@@ -73,13 +73,13 @@ public class AvlTreeTest {
 
     @Test
     public void test06() {
-        AvlTree AvlTree0 = new AvlTree();
-        AvlTree.Node node2 = AvlTree0.find((int) '4');
+        AvlTree AvlTree = new AvlTree();
+        AvlTree.Node node2 = AvlTree.find((int) '4');
         AvlTree.Node node3 = null;
-        int int4 = AvlTree0.getBalance(node3);
-        AvlTree0.insert((int) '4');
-        AvlTree.Node node8 = AvlTree0.find((int) (short) 0);
-        AvlTree0.delete(100);
+        int int4 = AvlTree.getBalance(node3);
+        AvlTree.insert((int) '4');
+        AvlTree.Node node8 = AvlTree.find((int) (short) 0);
+        AvlTree.delete(100);
         assertNull(node2);
         org.junit.Assert.assertTrue("'" + int4 + "' != '" + 0 + "'", int4 == 0);
         assertNull(node8);
@@ -87,10 +87,10 @@ public class AvlTreeTest {
 
     @Test
     public void test07() {
-        AvlTree AvlTree0 = new AvlTree();
-        AvlTree.Node node2 = AvlTree0.find((int) '4');
-        AvlTree.Node node3 = AvlTree0.getRoot();
-        AvlTree0.insert((int) (byte) 100);
+        AvlTree AvlTree = new AvlTree();
+        AvlTree.Node node2 = AvlTree.find((int) '4');
+        AvlTree.Node node3 = AvlTree.getRoot();
+        AvlTree.insert((int) (byte) 100);
         AvlTree AvlTree6 = new AvlTree();
         AvlTree.Node node8 = AvlTree6.find((int) '4');
         AvlTree.Node node9 = null;
@@ -99,8 +99,8 @@ public class AvlTreeTest {
         AvlTree.Node node14 = AvlTree6.find((int) ' ');
         int int15 = AvlTree6.height();
         AvlTree.Node node16 = AvlTree6.getRoot();
-        int int17 = AvlTree0.getBalance(node16);
-        java.lang.Class<?> wildcardClass18 = AvlTree0.getClass();
+        int int17 = AvlTree.getBalance(node16);
+        java.lang.Class<?> wildcardClass18 = AvlTree.getClass();
         assertNull(node2);
         assertNull(node3);
         assertNull(node8);
@@ -114,32 +114,32 @@ public class AvlTreeTest {
 
     @Test
     public void test08() {
-        AvlTree AvlTree0 = new AvlTree();
+        AvlTree AvlTree = new AvlTree();
         AvlTree.Node node1 = null;
-        int int2 = AvlTree0.getBalance(node1);
-        AvlTree0.insert((int) (byte) 100);
-        AvlTree0.delete((int) '4');
+        int int2 = AvlTree.getBalance(node1);
+        AvlTree.insert((int) (byte) 100);
+        AvlTree.delete((int) '4');
         org.junit.Assert.assertTrue("'" + int2 + "' != '" + 0 + "'", int2 == 0);
     }
 
     @Test
     public void test09() {
-        AvlTree avlTree = new AvlTree();
-        avlTree.insert(15);
-        avlTree.insert(17);
-        avlTree.insert(22);
-        avlTree.delete(22);
-        assertNull(avlTree.find(22));
+        AvlTree AvlTree = new AvlTree();
+        AvlTree.insert(15);
+        AvlTree.insert(17);
+        AvlTree.insert(22);
+        AvlTree.delete(22);
+        assertNull(AvlTree.find(22));
     }
 
     @Test
     public void test10() {
-        AvlTree avlTree = new AvlTree();
-        avlTree.insert(15);
-        avlTree.insert(12);
-        avlTree.insert(22);
-        avlTree.delete(15);
-        assertNull(avlTree.find(15));
+        AvlTree AvlTree = new AvlTree();
+        AvlTree.insert(15);
+        AvlTree.insert(12);
+        AvlTree.insert(22);
+        AvlTree.delete(15);
+        assertNull(AvlTree.find(15));
     }
 }
 
